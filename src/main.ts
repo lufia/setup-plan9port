58283c30f7e1ab0de37d143830c24bd82ae50fee
lufia <lufia@lufia.org>
2021-08-20 00:49:39 +0900

    rewrite installer

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,18 @@
+import tc from '@actions/tool-cache'
 import core from '@actions/core'
-import os from 'os'
-import path from 'path'
-import fetch from 'node-fetch'
-import {
-	createWriteStream,
-	promises as fs,
-} from 'fs'
-import { pipeline } from 'stream'
-import { promisify } from 'util'
 
-const streamPipeline = promisify(pipeline)
-
-async function downloadSource(branch: string): Promise<string> {
-	const dir = await fs.mkdtemp(path.join(os.tmpdir(), 'setup-plan9port-'))
+async function installFromSource(branch: string): Promise<string> {
 	const url = `https://github.com/9fans/plan9port/archive/refs/heads/${branch}.zip`;
-	const res = await fetch(url)
-	if(!res.ok)
-		throw new Error(`${url}: unexpected response: ${res.statusText}`)
-	const w = createWriteStream(path.join(dir, "plan9port.zip"))
-	await streamPipeline(res.body, w)
-	w.close()
-	return dir
+	const archivePath = await tc.downloadTool(url)
+	return await tc.extractZip(archivePath)
 }
 
 async function run(): Promise<void> {
 	try {
-		const ms: string = core.getInput('milliseconds')
-		core.debug(`Waiting ${ms} milliseconds ...`)
-
 		core.debug(new Date().toTimeString())
-		await downloadSource("master")
+		const dir = await installFromSource("master")
 		core.debug(new Date().toTimeString())
+		core.exportVariable('PLAN9', dir)
 
 		core.setOutput('time', new Date().toTimeString())
 	} catch (error) {
