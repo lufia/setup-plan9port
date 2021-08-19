ff5270c29134858b92f03389f89716139d129aa4
lufia <lufia@lufia.org>
2021-08-20 00:23:43 +0900

    download source archive from GitHub

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
-import * as core from '@actions/core'
+import core from '@actions/core'
+import os from 'os'
+import path from 'path'
+import fetch from 'node-fetch'
+import {
+	createWriteStream,
+	promises as fs,
+} from 'fs'
+import { pipeline } from 'stream'
+import { promisify } from 'util'
 
-async function downloadSource() {
+const streamPipeline = promisify(pipeline)
+
+async function downloadSource(branch: string): Promise<string> {
+	const dir = await fs.mkdtemp(path.join(os.tmpdir(), 'setup-plan9port-'))
+	const url = `https://github.com/9fans/plan9port/archive/refs/heads/${branch}.zip`;
+	const res = await fetch(url)
+	if(!res.ok)
+		throw new Error(`${url}: unexpected response: ${res.statusText}`)
+	const w = createWriteStream(path.join(dir, "plan9port.zip"))
+	await streamPipeline(res.body, w)
+	w.close()
+	return dir
 }
 
 async function run(): Promise<void> {
@@ -9,6 +29,7 @@ async function run(): Promise<void> {
 		core.debug(`Waiting ${ms} milliseconds ...`)
 
 		core.debug(new Date().toTimeString())
+		await downloadSource("master")
 		core.debug(new Date().toTimeString())
 
 		core.setOutput('time', new Date().toTimeString())
