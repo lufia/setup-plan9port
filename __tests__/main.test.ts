7dc40e6ebd00d592da2a757c0b0e285a5f90f0e9
lufia <lufia@lufia.org>
2025-02-23 23:12:12 +0900

    fix tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,20 +1,24 @@
 import * as core from '@actions/core'
+import { exec } from '@actions/exec'
+import * as tool from '@actions/tool-cache'
 import { run } from '../src/main'
 
 let debugMock: jest.SpiedFunction<typeof core.debug>
 let getInputMock: jest.SpiedFunction<typeof core.getInput>
 let setFailedMock: jest.SpiedFunction<typeof core.setFailed>
 let exportVariableMock: jest.SpiedFunction<typeof core.exportVariable>
+let downloadToolMock: jest.SpiedFunction<typeof tool.downloadTool>
 
 describe('action', () => {
 	beforeEach(() => {
 		jest.clearAllMocks()
 		debugMock = jest.spyOn(core, 'debug').mockImplementation()
 		getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
-		getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
+		setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
 		exportVariableMock = jest
 			.spyOn(core, 'exportVariable')
 			.mockImplementation()
+		downloadToolMock = jest.spyOn(tool, 'downloadTool').mockImplementation()
 	})
 
 	it('test runs', async () => {
@@ -25,6 +29,12 @@ describe('action', () => {
 				default:
 					return ''
 			}
+		})
+		downloadToolMock.mockImplementation(() => {
+			const dir = import.meta.dirname
+			const file = path.join(dir, 'mock.tar.gz')
+			exec(`tar zcf mock.tar.gz -C "${dir}" plan9`)
+			return file
 		})
 		await run()
 		expect(getInputMock).toHaveBeenCalledWith('environment')
